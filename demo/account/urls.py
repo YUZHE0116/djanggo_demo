3e669f6e410ebd1ed9af6730a43c4a130261bd7d
Zhang0116 <3315615171@qq.com>
2025-08-07 23:13:47 +0800

    添加注册功能，创建注册视图和模板，更新路由配置

--- demo/account/urls.py
+++ demo/account/urls.py
@@ -3,4 +3,5 @@ from . import views
 urlpatterns = [
     path('login/', views.login),
     # 在这里添加你的URL模式
+    path('register/', views.RegisterView.as_view()),
 ]
--- demo/account/views.py
+++ demo/account/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import View
 # Create your views here.
 def login(request):
     if request.method == "POST":
@@ -7,5 +8,18 @@ def login(request):
         password = request.POST.get("password")
         return HttpResponse(f"用户名：{username}, 密码：{password}")
     elif request.method == "GET":
+        FORBIDDEN_IP= ['127.0.0.1']
+        print(request.META.get('REMOTE_ADDR'))
+        if request.META.get('REMOTE_ADDR') in FORBIDDEN_IP:
+            return HttpResponse("禁止访问")
         return render(request, 'login.html')
+class RegisterView(View):
+    def get(self, request):
+        return render(request, 'register.html')
+
+    def post(self, request):
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        confirm_password = request.POST.get("confirm_password")
+        return HttpResponse(f"注册成功！用户名：{username}, 密码：{password}")
 
